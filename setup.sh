4d34ee6cce387ec36de1848a131f3a623e2fb90c
Tyelr A Stank <tylerastank@gmail.com>
2023-07-04 21:08:51 -0500

    Rename setup.sh appropriately, uncomment cloning the raylib repo, build the demo app.

--- setup.sh
+++ setup.sh
@@ -4,13 +4,17 @@
 PWD=`pwd`
 # the libraries after the # are needed only on desktop platform installs
 sudo apt install make git vim cmake libgles2-mesa-dev libgbm-dev libdrm-dev -y # libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
-#git clone https://github.com/raysan5/raylib.git
+git clone https://github.com/raysan5/raylib.git
 cd raylib
 # choose one depending on environment
 #cmake -DPLATFORM=Desktop -DOPENGL_VERSION=2.1 -DPLATFORM_CPP=PLATFORM_DESKTOP -DGRAPHICS=GRAPHICS_API_OPENGL_21 .
 cmake -DPLATFORM=DRM -DOPENGL_VERSION="ES 2.0" -DPLATFORM_CPP=PLATFORM_DRM -DGRAPHICS=GRAPHICS_API_OPENGL_ES2 .
 make
 sudo make install
+
+# Build the demo program
+cd $PWD
+make
 
 # Make it run at boot time
 SERVICE_FILE=/lib/systemd/system/cluster.service
