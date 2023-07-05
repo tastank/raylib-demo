c05d0499a577564b63f66901c416edf128d437ef
Tyelr A Stank <tylerastank@gmail.com>
2023-07-04 21:26:56 -0500

    Add progress markers, don't use special variable PWD

--- setup.sh
+++ setup.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 
 # Install raylib and its dependencies
-PWD=`pwd`
+START_DIR=`pwd`
 # the libraries after the # are needed only on desktop platform installs
-sudo apt install make git vim cmake libgles2-mesa-dev libgbm-dev libdrm-dev -y # libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
+echo "================Installing Raylib and its dependencies================"
+#sudo apt install make git vim cmake libgles2-mesa-dev libgbm-dev libdrm-dev -y # libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
 git clone https://github.com/raysan5/raylib.git
 cd raylib
 # choose one depending on environment
 #cmake -DPLATFORM=Desktop -DOPENGL_VERSION=2.1 -DPLATFORM_CPP=PLATFORM_DESKTOP -DGRAPHICS=GRAPHICS_API_OPENGL_21 .
 cmake -DPLATFORM=DRM -DOPENGL_VERSION="ES 2.0" -DPLATFORM_CPP=PLATFORM_DRM -DGRAPHICS=GRAPHICS_API_OPENGL_ES2 .
-make
-sudo make install
+#make
+#sudo make install
 
 # Build the demo program
-cd $PWD
+echo "================Building demo program================"
+cd $START_DIR
 make
 EXEC_FILE=raylib-demo
 
 # Make it run at boot time
+echo "================Creating and enabling service================"
 SERVICE_FILE=/lib/systemd/system/$EXEC_FILE.service
 touch $SERVICE_FILE
 echo "[Unit]" > $SERVICE_FILE
 echo "Description=Raylib Demo" >> $SERVICE_FILE
 echo "After=multi-user.target\n" >> $SERVICE_FILE
 echo "[Service]" >> $SERVICE_FILE
-echo "ExecStart=$PWD/$EXEC_FILE\n" >> $SERVICE_FILE
+echo "ExecStart=$START_DIR/$EXEC_FILE\n" >> $SERVICE_FILE
 echo "[Install]" >> $SERVICE_FILE
 echo "WantedBy=multi-user.target" >> $SERVICE_FILE
-sudo systemctl daemon-reload
-sudo systemctl enable $EXEC_FILE.service
+systemctl daemon-reload
+systemctl enable $EXEC_FILE.service
